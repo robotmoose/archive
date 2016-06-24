04d92f92d34e7c855e89882cabc2672230d2422e
Ryker Dial <ryker.dial@gmail.com>
2016-06-24 13:20:45 -0800

    Cleaned and organized code, expanded functionallity

--- sensors/sound_localize/software/Sound_Interface.cpp
+++ sensors/sound_localize/software/Sound_Interface.cpp
@@ -4,7 +4,7 @@
 // UAF ITEST
 
 // Date Created: 5/31/2016
-// Last Modified: 6/23/2016
+// Last Modified: 6/24/2016
 
 #include <string>
 #include <cstdint>
@@ -20,45 +20,61 @@
 #include <fstream>
 #include <chrono> // For timing
 
-#define NUMSAMPLES_FFT 2271 // Number of samples to use for FFT. FFT resolution is fs/N.
+#define NUMSAMPLES_FFT 2771 // Number of samples to use for FFT. FFT resolution is fs/N.
 #define FS 11025.0 // Sampling frequency is twice max frequency
 
 // Function Prototypes
-bool serial_init(serial::Serial & sp);
-void make_xcor_plans(fftw_plan & pa, fftw_plan & pb, fftw_plan & px);
+bool SerialInit(serial::Serial & sp);
+void MakeXcorPlans(fftw_plan &, fftw_plan &);
+std::pair<double, int> FindMaxCorrPair(fftw_complex *, fftw_complex *, fftw_complex *, fftw_plan &, fftw_plan &);
+void PrintXcorrResult(std::pair<double, int>, int, int);
 
 int main() {
 	// Setup and open the serial port.
 	//serial::Serial sp;
-	//serial_init(sp);
+	//SerialInit(sp);
 
-	fftw_complex * signala = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * NUMSAMPLES_FFT);
-	fftw_complex * signalb = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * NUMSAMPLES_FFT);
+	// ***** DSP Section: Sound Source Localization ***** // 
+	// TODO: Scale this section up so it can handle an arbitrary number of input signals
+	fftw_complex * signal1 = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * NUMSAMPLES_FFT);
+	fftw_complex * signal2 = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * NUMSAMPLES_FFT);
+	fftw_complex * signal3 = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * NUMSAMPLES_FFT);
 	fftw_complex * result = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * 2*NUMSAMPLES_FFT-1);
 
-	std::ifstream s1File ("s1.txt");
-	std::ifstream s2File ("s2.txt");
-
-	std::ofstream output ("output.csv", std::ios::out | std::ios::trunc);
+	std::ifstream s1File ("../test_data/s1.txt");
+	std::ifstream s2File ("../test_data/s2.txt");
+	std::ifstream s3File ("../test_data/s3.txt");
 
 	for(int i=0; i<NUMSAMPLES_FFT; ++i) {
 		std::string value;
 		std::getline(s1File, value);
-		signala[i][0] = std::stod(value);
+		signal1[i][0] = std::stod(value);
 		std::getline(s2File, value);
-		signalb[i][0] = std::stod(value);
+		signal2[i][0] = std::stod(value);
+		std::getline(s3File, value);
+		signal3[i][0] = std::stod(value);
 	}
 
 	// Since ffts and iffts will always be of the same length and type, create the plans only once to boost efficiency.
-    fftw_plan pa, pb, px;
-    make_xcor_plans(pa,pb,px); // pa and pb are fft of signal a and signal b, and px is ifft of xcor of a & b
+    fftw_plan fft, ifft;
+    MakeXcorPlans(fft, ifft);
+
+	std::pair<double, int> max_corr_pair_12, max_corr_pair_13, max_corr_pair_23;
+	max_corr_pair_12 = FindMaxCorrPair(signal1, signal2, result, fft, ifft);
+	PrintXcorrResult(max_corr_pair_12, 1, 2);
+	max_corr_pair_13 = FindMaxCorrPair(signal1, signal3, result, fft, ifft);
+	PrintXcorrResult(max_corr_pair_13, 1, 3);
+	max_corr_pair_23 = FindMaxCorrPair(signal2, signal3, result, fft, ifft);
+	PrintXcorrResult(max_corr_pair_23, 2, 3);
+	// ***** End DSP Section ***** //
+
 
 	// Benchmark cross correlation
-	std::uint64_t avg = 0;
+	/*std::uint64_t avg = 0;
 	std::uint32_t numiters = 1000;
 	for(int i=0; i<numiters; ++i) {
 		auto begin = std::chrono::high_resolution_clock::now();
-		xcorr(signala, signalb, result, NUMSAMPLES_FFT, pa, pb, px);
+		xcorr(signala, signalb, result, NUMSAMPLES_FFT, fft, ifft);
 		auto end = std::chrono::high_resolution_clock::now();
 		avg += std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
 	}
@@ -76,11 +92,15 @@ int main() {
 	std::cout << "Max correlation is " << max_corr_pair.first << " at a lag of " << max_corr_pair.second << std::endl;
 	if(max_corr_pair.second>0) std::cout << "Thus, signal a leads signal b by " << max_corr_pair.second/FS << " seconds\n";
 	else if(max_corr_pair.second<0) std::cout << "Thus, signal a lags signal b by " << max_corr_pair.second/FS << " seconds\n";
-	else std::cout << "Thus, signals a and b are fully synchronized\n";
+	else std::cout << "Thus, signals a and b are fully synchronized\n";*/
+
 	// Clean up
-	fftw_destroy_plan(pa);
-	fftw_destroy_plan(pb);
-	fftw_destroy_plan(px);
+	fftw_free(signal1);
+	fftw_free(signal2);
+	fftw_free(signal3);
+	fftw_free(result);
+	fftw_destroy_plan(fft);
+	fftw_destroy_plan(ifft);
 	fftw_cleanup();
 
 	// ********** Data Parsing and Processing Setup Begin ********** // 
@@ -158,37 +178,6 @@ int main() {
 	// 				//std::printf("Microphone data %d: %u\n", i, microphone_data[i]);
 	// 			}
 	// 			//std::printf("Counter: %lu\n", fft_counter);
-	// 			if(fft_counter >= NUMSAMPLES_FFT) {
-	// 				// Execute the Fourier transforms
-	// 				for(int i=0; i<num_streams; ++i) {
-	// 					for(int j=0; j<NUMSAMPLES_FFT; ++j) {
-	// 						// Copy microphone data over. This will convert int to double.
-	// 						fft_data[i].first[j] = (double) data_streams[i][j];
-	// // 					}
-	// 					fftw_execute_r2r(fft, fft_data[i].first, fft_data[i].second);
-	// 				}
-	// 				// Log the FFT outputs.
-	// 				for(int i=0; i<(NUMSAMPLES_FFT+1)/2; ++i) {
-	// 					for(int j=0; j<num_streams; ++j) {
-	// 						std::sprintf(buff, "%f,", fft_data[j].second[i]);
-	// 						output_freq << buff;			
-	// 					}
-	// 					output_freq << "\n";
-	// 				}
-
-	// 				// // For each adjacent FFT pair, calculate the cross correlation.
-	// 				// for(int i=0; i<num_streams+1; ++i) {
-	// 				// 	if(i!=num_streams) {
-	// 				// 		for(int j=0; j<NUMSAMPLES_FFT; ++j) {
-	// 				// 			crossCorr_pairs[i][j] = std::abs(fft_data[i].second[j]*fft_data[i+1].second[j]);
-	// 				// 		}
-	// 				// 	}
-	// 				// 	else { // Wraparound
-	// 				// 		for(int j=0; j<NUMSAMPLES_FFT; ++j) {
-	// 				// 			crossCorr_pairs[i][j] = std::abs(fft_data[i].second[j]*fft_data[0].second[j]);
-	// 				// 		}
-	// 				// 	}
-	// 				// }
 
 	// 				++counter;
 	// 				std::printf("FFT Counter %lu\n", counter);
@@ -209,7 +198,10 @@ int main() {
 	return 0;
 }
 
-bool serial_init(serial::Serial & sp) {
+
+// ********** Helper Functions for Sound Source Localization Program ********** //
+
+bool SerialInit(serial::Serial & sp) {
 	// Serial Port Parameters
 	std::string port = "";
 	std::uint32_t baudrate = 921600;
@@ -254,27 +246,42 @@ bool serial_init(serial::Serial & sp) {
 	}
 }
 
-void make_xcor_plans(fftw_plan & pa, fftw_plan & pb, fftw_plan & px) {
-    fftw_complex * signala_ext = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * (2 * NUMSAMPLES_FFT - 1));
-    fftw_complex * signalb_ext = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * (2 * NUMSAMPLES_FFT - 1));
-    fftw_complex * out_shifted = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * (2 * NUMSAMPLES_FFT - 1));
-    fftw_complex * outa = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * (2 * NUMSAMPLES_FFT - 1));
-    fftw_complex * outb = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * (2 * NUMSAMPLES_FFT - 1));
+void MakeXcorPlans(fftw_plan & fft, fftw_plan & ifft) {
+    fftw_complex * signal_ext = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * (2 * NUMSAMPLES_FFT - 1));
     fftw_complex * out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * (2 * NUMSAMPLES_FFT - 1));
-    fftw_complex * result = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * 2*NUMSAMPLES_FFT-1);
 
-    pa = fftw_plan_dft_1d(2 * NUMSAMPLES_FFT - 1, signala_ext, outa, FFTW_FORWARD, FFTW_MEASURE);
-    pb = fftw_plan_dft_1d(2 * NUMSAMPLES_FFT - 1, signalb_ext, outb, FFTW_FORWARD, FFTW_MEASURE);
-    px = fftw_plan_dft_1d(2 * NUMSAMPLES_FFT - 1, out, result, FFTW_BACKWARD, FFTW_MEASURE);
+    fft = fftw_plan_dft_1d(2 * NUMSAMPLES_FFT - 1, signal_ext, out, FFTW_FORWARD, FFTW_MEASURE);
+   	ifft = fftw_plan_dft_1d(2 * NUMSAMPLES_FFT - 1, out, signal_ext, FFTW_BACKWARD, FFTW_MEASURE);
 
-    fftw_free(signala_ext);
-    fftw_free(signalb_ext);
-    fftw_free(out_shifted);
+    fftw_free(signal_ext);
     fftw_free(out);
-    fftw_free(outa);
-    fftw_free(outb);
+}
+
+std::pair<double, int> FindMaxCorrPair(fftw_complex * signala, fftw_complex * signalb, fftw_complex * result, fftw_plan & fft, fftw_plan & ifft) {
+	xcorr(signala, signalb, result, NUMSAMPLES_FFT, fft, ifft);
+
+	std::pair<double, int> max_corr_pair = std::make_pair(result[0][0],-NUMSAMPLES_FFT+1);
+	for(int i=0; i<2*NUMSAMPLES_FFT-1; ++i) {
+		if(result[i][0] > max_corr_pair.first) {
+			max_corr_pair.first = result[i][0];
+			max_corr_pair.second = i-NUMSAMPLES_FFT+1; 
+		}
+	}
+	return max_corr_pair;
+}
+
+void PrintXcorrResult(std::pair<double, int> max_corr_pair, int signum1, int signum2) {
+	std::printf("Max correlation of signals %d and %d is %f at a lag of %d.\n", signum1, signum2, max_corr_pair.first, max_corr_pair.second);
+	if(max_corr_pair.second > 0) 
+		std::printf("Signal %d leads signal %d by %f seconds.\n", signum1, signum2, max_corr_pair.second/FS);
+	else if(max_corr_pair.second < 0)
+		std::printf("Signal %d lags signal %d by %f seconds.\n", signum1, signum2, max_corr_pair.second/FS);
+	else
+		std::printf("Signals %d and %d are fully synchronized.\n", signum1, signum2);
 }
 
 // void parse_serial() {
 	
 // }
+
+// ********** End Helper Functions Section ********** //
